5852b08779adcc4603798bcc3b701bc358f7edf8
vova_nikon <obsolutly@gmail.com>
2021-02-08 16:05:47 +0800

    after hw

--- p_library/admin.py
+++ p_library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from p_library.models import Book, Author, Publisher
+from p_library.models import Book, Author, Publisher, Friend
 
 # Register your models here.
 
@@ -9,7 +9,7 @@ class BookAdmin(admin.ModelAdmin):
     def author_full_name(obj):
         return obj.author.full_name
     list_display = ('title', 'author_full_name')
-    fields = ('ISBN', 'title', 'publisher', 'description', 'year_release', 'author', 'price', 'copy_count')
+    fields = ('ISBN', 'title', 'publisher', 'description', 'year_release', 'author', 'price', 'copy_count', 'friend')
 
 
 @admin.register(Author)
@@ -18,4 +18,9 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Friend)
+
+class FriendAdmin(admin.ModelAdmin):
     pass
--- p_library/models.py
+++ p_library/models.py
@@ -14,6 +14,11 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
+class Friend(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):  
     ISBN = models.CharField(max_length=13)  
     title = models.TextField()
@@ -24,6 +29,7 @@ class Book(models.Model):
     copy_count = models.PositiveSmallIntegerField(default=1)
     price = models.DecimalField(max_digits=999, decimal_places=2, default=1)
     publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE, null=True, blank=True, related_name='books')
+    friend = models.ForeignKey('Friend', on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.title
 
